5a360c5b8aa8541580021105726d1753d206100c
RareSpeciment <davidrafael0987@gmail.com>
2025-09-02 11:40:04 +0700

    Update githubtrial.java dan tambah contributors.java

---
+++ Semester 3 dan 4/Tugas PBO/contributors.java
@@ -0,0 +1,6 @@
+public class contributors {
+    public static void main(String[] args) {
+        String kol = "aku suka kok";
+        System.out.println(kol);
+    }
+}
--- Semester 3 dan 4/Tugas PBO/githubtrial.java
+++ Semester 3 dan 4/Tugas PBO/githubtrial.java
@@ -1,5 +1,5 @@
 public class githubtrial {
     public static void main(String[] args) {
-        
+        System.out.println("Alamak");
     }
 }
